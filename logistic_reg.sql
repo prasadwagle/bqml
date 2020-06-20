2a67633e56ff46da1f99602ec8a66979c60777ea
Prasad Wagle <prasadwagle@gmail.com>
2020-06-19 20:50:50 -0700

    Update logistic_reg.sql

--- logistic_reg.sql
+++ logistic_reg.sql
@@ -1,4 +1,4 @@
-CREATE OR REPLACE MODEL pwagle.txtclass
+CREATE OR REPLACE MODEL pwagle.txtclass_logistic_reg
 OPTIONS(model_type='logistic_reg', input_label_cols=['source'])
 AS
 WITH extracted AS (
