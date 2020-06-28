d60f2a7e5784065c5764262ab4dd827f02ec102d
Prasad Wagle <prasadwagle@gmail.com>
2020-06-28 13:39:07 -0700

    Update logistic_reg.sql

--- logistic_reg.sql
+++ logistic_reg.sql
@@ -2,24 +2,5 @@ CREATE OR REPLACE MODEL pwagle.txtclass_logistic_reg
 OPTIONS(model_type='logistic_reg', input_label_cols=['source'])
 AS
 WITH extracted AS (
-SELECT source, REGEXP_REPLACE(LOWER(REGEXP_REPLACE(title, '[^a-zA-Z0-9 $-]', ' ')), "  ", " ") AS title 
-FROM
-  (SELECT
-    ARRAY_REVERSE(SPLIT(REGEXP_EXTRACT(url, '.*://(.[^/]+)/'), '.'))[OFFSET(1)] AS source,
-    title
-  FROM
-    `bigquery-public-data.hacker_news.stories`
-  WHERE
-    REGEXP_CONTAINS(REGEXP_EXTRACT(url, '.*://(.[^/]+)/'), '.com$')
-    AND LENGTH(title) > 10
-  )
-)
-, ds AS (
-SELECT SPLIT(title, " ") AS words, source 
-FROM extracted
-WHERE (source = 'github' OR source = 'nytimes' OR source = 'techcrunch')
-)
-SELECT 
-source, 
-words
-FROM ds
+SELECT source, title
+FROM ds pwagle.txtclass_input
